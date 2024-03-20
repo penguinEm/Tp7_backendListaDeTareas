2182dffed8046a3b7c7fc3b24061979dd8431364
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-20 13:51:40 -0300

    Configuramos un archivo estatico en la ruta principal

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path"
+import { fileURLToPath } from "url";
 
 //! 1. CONFIGURAR UN PUERTO
 const app = express();
-app.set("port", process.env.PORT || 4000)
-app.listen(app.get("port"), () =>{
-    console.log(`Estoy en el puerto ` + app.get("port"))
-})
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log(`Estoy en el puerto ` + app.get("port"));
+});
 
 //! 2. CONFUGURAR LOS MIDDLEWARES
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, "/public")))
+
 
 //! 3. CONFIGURAR LAS RUTAS
+app.get("/nuevo", (req, res) => {
+  console.log("procesando solicitud get");
+  res.send("respuesta del backend");
+});
