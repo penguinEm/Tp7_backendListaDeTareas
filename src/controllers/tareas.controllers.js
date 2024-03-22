5d83dc9e0b7d8692ff8d15836cf4bf315dead127
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-21 21:12:46 -0300

    Se agrego la logica para buscar 1 tarea mediante su id

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -24,3 +24,21 @@ export const crearTareaDb = async (req, res) => {
     res.status(400).json({ mensaje: "La tarea no pudo ser creada" });
   }
 };
+
+//! GET de 1 tarea buscada por id
+export const tareaBuscadaDb = async (req, res, id) => {
+  try {
+    const tareaBuscada = await Tarea.findById(req.params.id);
+    if (tareaBuscada === null) {
+      return res
+        .status(404)
+        .json({ mensaje: "No existe el producto con el id enviado" });
+    }
+    res.status(200).json({ mensaje: tareaBuscada });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ mensaje: "No se pudo encontrar solicitado, id incorrecto" });
+  }
+};
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import {
   crearTareaDb,
   listarTareasDb,
+  tareaBuscadaDb,
 } from "../controllers/tareas.controllers.js";
 const router = Router();
 
 router.route("/tareas").get(listarTareasDb).post(crearTareaDb);
+router.route("/tareas/:id").get(tareaBuscadaDb)
 
 export default router;
