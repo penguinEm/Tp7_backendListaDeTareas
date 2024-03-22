8f8cbbf1006f55740554636820c6b5f3c0e00a7d
emir paradi <emir.alejandro.paradi@gmail.com>
2024-03-21 21:00:57 -0300

    Se agrego la logica para listar tareas en la ruta /api/tareas

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,19 +1,24 @@
-import Tarea from "../database/model/modelTarea.js"
+import Tarea from "../database/model/modelTarea.js";
 
 //! GET de todas las tareas
-export const listarTareasDb = (req, res) => {
-  console.log("logica para listar tareas");
-  res.send("enviando lista de tareas");
+export const listarTareasDb = async (req, res) => {
+  try {
+    const listaTareas = await Tarea.find({});
+    res.status(200).json(listaTareas);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al listar tareas" });
+  }
 };
 
 //! POST crear tareas
 export const crearTareaDb = async (req, res) => {
   try {
-    const tareaNueva = new Tarea(req.body)
-    await tareaNueva.save()
+    const tareaNueva = new Tarea(req.body);
+    await tareaNueva.save();
     res.status(201).json({
-      mensaje: "La tarea fue creada exitosamente"
-    })
+      mensaje: "La tarea fue creada exitosamente",
+    });
   } catch (error) {
     console.error(error);
     res.status(400).json({ mensaje: "La tarea no pudo ser creada" });
